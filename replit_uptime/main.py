202a90dedd876aefcf9fcdb8ca4b6cd4ad0de6a0
coshelev <coshelev@gmail.com>
2023-03-31 15:09:55 +0300

    http responce code

--- replit_uptime/main.py
+++ replit_uptime/main.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import requests
+
 app = Flask('app')
 
+#########################################
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  #return 'Hello, World!'
+
+  badstatus = ''
+  url = 'https://www.example.com'
+  response = requests.get(url)
+  status_code = response.status_code
+  if status_code != 200:
+    badstatus = badstatus+"; "+url
+
+  if len(badstatus) != 0:
+    return 'bad status: ' + badstatus
+
+  return "ok"
+###########################################
+
 
 app.run(host='0.0.0.0', port=8080)
-
